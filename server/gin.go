1b56e698cb369d34b67791b0ac5ca782a31e80eb
TimIsOverpowered <admin@angelthump.com>
2024-06-08 05:55:50 -0500

    revert again for testing

--- server/gin.go
+++ server/gin.go
@@ -1,17 +1,13 @@
 package server
 
 import (
-	"encoding/json"
-	"log"
 	"regexp"
 	"strings"
 	"time"
 
-	"github.com/angelthump/cache-main/api"
 	"github.com/angelthump/cache-main/client"
 	utils "github.com/angelthump/cache-main/utils"
 	"github.com/gin-gonic/gin"
-	"github.com/go-resty/resty/v2"
 )
 
 func Initalize() {
@@ -40,7 +36,7 @@ func Initalize() {
 		} else if strings.HasSuffix(endUrl, ".m3u8") {
 			client.Rdb.Set(client.Ctx, key, data, 16*time.Second)
 		} else if strings.HasSuffix(endUrl, "init.mp4") {
-			client.Rdb.Set(client.Ctx, key, data, 16*time.Second)
+			client.Rdb.Set(client.Ctx, key, data, 24*time.Hour)
 		} else if strings.HasSuffix(endUrl, ".mp4") {
 			client.Rdb.Set(client.Ctx, key, data, 20*time.Second)
 		} else if strings.HasSuffix(endUrl, ".m4s") {
@@ -69,7 +65,7 @@ func Initalize() {
 		} else if strings.HasSuffix(endUrl, ".m3u8") {
 			client.Rdb.Set(client.Ctx, key, data, 16*time.Second)
 		} else if strings.HasSuffix(endUrl, "init.mp4") {
-			client.Rdb.Set(client.Ctx, key, data, 16*time.Second)
+			client.Rdb.Set(client.Ctx, key, data, 24*time.Hour)
 		} else if strings.HasSuffix(endUrl, ".mp4") {
 			client.Rdb.Set(client.Ctx, key, data, 20*time.Second)
 		} else if strings.HasSuffix(endUrl, ".m4s") {
@@ -103,59 +99,62 @@ func Initalize() {
 			return
 		}
 
-		regex := regexp.MustCompile(`_src|_medium|_low`)
-		base64Channel := regex.ReplaceAllString(c.Param("channel"), "")
+		c.AbortWithStatus(404)
 
-		data, err = client.Rdb.Get(client.Ctx, base64Channel).Result()
-		if err != nil {
-			c.AbortWithStatus(400)
-			return
-		}
+		/*
+			regex := regexp.MustCompile(`_src|_medium|_low`)
+			base64Channel := regex.ReplaceAllString(c.Param("channel"), "")
 
-		var streamData api.Stream
-		err = json.Unmarshal([]byte(data), &streamData)
-		if err != nil {
-			log.Printf("Unmarshal Error %v", err)
-			c.AbortWithStatus(400)
-			return
-		}
+			data, err = client.Rdb.Get(client.Ctx, base64Channel).Result()
+			if err != nil {
+				c.AbortWithStatus(400)
+				return
+			}
 
-		if !streamData.Ingest.Mediamtx {
-			c.AbortWithStatus(404)
-			return
-		}
+			var streamData api.Stream
+			err = json.Unmarshal([]byte(data), &streamData)
+			if err != nil {
+				log.Printf("Unmarshal Error %v", err)
+				c.AbortWithStatus(400)
+				return
+			}
 
-		restyClient := resty.New()
-		resp, _ := restyClient.R().
-			SetHeader("X-Api-Key", utils.Config.StreamsAPI.AuthKey).
-			Get("https://" + utils.Config.IngestAPI.Username + ":" + utils.Config.IngestAPI.Password + "@" + streamData.Ingest.Server + ".angelthump.com/hls/live/" + streamData.User.Username + "/" + endUrl)
+			if !streamData.Ingest.Mediamtx {
+				c.AbortWithStatus(404)
+				return
+			}
 
-		statusCode := resp.StatusCode()
-		if statusCode >= 400 {
-			c.AbortWithStatus(400)
-			return
-		}
+			restyClient := resty.New()
+			resp, _ := restyClient.R().
+				SetHeader("X-Api-Key", utils.Config.StreamsAPI.AuthKey).
+				Get("https://" + utils.Config.IngestAPI.Username + ":" + utils.Config.IngestAPI.Password + "@" + streamData.Ingest.Server + ".angelthump.com/hls/live/" + streamData.User.Username + "/" + endUrl)
 
-		c.Header("Access-Control-Allow-Origin", "*")
+			statusCode := resp.StatusCode()
+			if statusCode >= 400 {
+				c.AbortWithStatus(400)
+				return
+			}
 
-		if strings.HasSuffix(endUrl, ".ts") {
-			client.Rdb.Set(client.Ctx, key, resp.Body(), 20*time.Second)
-			c.Data(200, "video/mp2t", []byte(resp.Body()))
-		} else if strings.HasSuffix(endUrl, "init.mp4") {
-			client.Rdb.Set(client.Ctx, key, data, 1*time.Second)
-			c.Data(200, "video/mp4", []byte(resp.Body()))
-		} else if strings.HasSuffix(endUrl, ".mp4") {
-			client.Rdb.Set(client.Ctx, key, resp.Body(), 20*time.Second)
-			c.Data(200, "video/mp4", []byte(resp.Body()))
-		} else if strings.HasSuffix(endUrl, ".m4s") {
-			client.Rdb.Set(client.Ctx, key, resp.Body(), 20*time.Second)
-			c.Data(200, "video/mp4", []byte(resp.Body()))
-		} else if strings.HasSuffix(endUrl, ".m3u8") {
-			client.Rdb.Set(client.Ctx, key, resp.Body(), 1*time.Second)
-			c.Data(200, "application/x-mpegURL", []byte(resp.Body()))
-		} else {
-			c.AbortWithStatus(400)
-		}
+			c.Header("Access-Control-Allow-Origin", "*")
+
+			if strings.HasSuffix(endUrl, ".ts") {
+				client.Rdb.Set(client.Ctx, key, resp.Body(), 20*time.Second)
+				c.Data(200, "video/mp2t", []byte(resp.Body()))
+			} else if strings.HasSuffix(endUrl, "init.mp4") {
+				client.Rdb.Set(client.Ctx, key, data, 1*time.Second)
+				c.Data(200, "video/mp4", []byte(resp.Body()))
+			} else if strings.HasSuffix(endUrl, ".mp4") {
+				client.Rdb.Set(client.Ctx, key, resp.Body(), 20*time.Second)
+				c.Data(200, "video/mp4", []byte(resp.Body()))
+			} else if strings.HasSuffix(endUrl, ".m4s") {
+				client.Rdb.Set(client.Ctx, key, resp.Body(), 20*time.Second)
+				c.Data(200, "video/mp4", []byte(resp.Body()))
+			} else if strings.HasSuffix(endUrl, ".m3u8") {
+				client.Rdb.Set(client.Ctx, key, resp.Body(), 1*time.Second)
+				c.Data(200, "application/x-mpegURL", []byte(resp.Body()))
+			} else {
+				c.AbortWithStatus(400)
+			}*/
 	})
 
 	router.Run(":" + utils.Config.Port)
